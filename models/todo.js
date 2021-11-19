a219658320e0b095efe482f1fb73902f1d1ea190
unknown <blshadi@gmail.com>
2021-11-19 10:58:24 +0200

    Modify some filed of Todo schema to be required

--- models/todo.js
+++ models/todo.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var todoSchema = new Schema({  
     userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    username: {type: String},
-    itemname: {type: String},
+    username: {type: String, required : true},
+    itemname: {type: String, required : true},
     category:{type: String, enum: ['category-a', 'category-b','category-c']},        
     date: {type: Date, default: Date.now}
 });
